2836ca135da1128f6039ad42efb94c738d41fb4e
Fabiano França <email@fabianofranca.com>
2017-04-23 22:18:40 -0300

    Setup colors, icons and slash screen

--- app/src/main/java/com/fabianofranca/androidlab/MainActivity.java
+++ app/src/main/java/com/fabianofranca/androidlab/MainActivity.java
@@ -7,7 +7,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Remove splash theme
         setTheme(R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
